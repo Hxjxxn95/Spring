c584cda47a79a45d858a64865dd7417d35f0e88c
Hojoon Kim <hojoon905@naver.com>
2023-08-14 17:10:21 +0900

    스프링 핵심 원리 기본편
    # Conflicts:
    #	.idea/gradle.xml

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 @RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 자동으로 만들어줌
 public class OrderServiceImpl implements OrderService {
@@ -15,13 +16,12 @@ public class OrderServiceImpl implements OrderService {
     // 테스트 용도
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // -> DIP, OCP 위반
-    @Getter
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // -> DIP, OCP 지킴
 
-
 //    @Autowired //"REQUIREDArgsConstructor" 가 있어서 필요 없음
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
@@ -31,8 +31,4 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.dicount(member, itemPrice); // 할인 정책에 회원 정보를 넘겨서 할인 가격을 받아온다.
         return new Order(memberId, itemName, itemPrice, discountPrice); // 주문 생성
     }
-    // 테스트 용도
-//    public DiscountPolicy getDiscountPolicy() {
-//        return discountPolicy;
-//    }
 }
